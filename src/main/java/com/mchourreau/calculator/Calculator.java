d150d31f1b49840b5cfb89f3653a11efd677fe6a
CHOURREAU Mathieu <mchourreau@iutbayonne.univ-pau.fr>
2024-09-30 15:40:57 +0200

    ajout des premiers tests d'entier d'ajout et de division

--- src/main/java/com/mchourreau/calculator/Calculator.java
+++ src/main/java/com/mchourreau/calculator/Calculator.java
@@ -1,4 +1,14 @@
 package com.mchourreau.calculator;
 
 public class Calculator {
+    // Méthode pour l'addition
+    public int add(int opG, int opD) {
+        return opG + opD;
+    }
+
+    // Méthode pour la division
+    public int divide(int opG, int opD) {
+        return opG / opD;
+    }
+
 }
--- src/test/java/com/mchourreau/calculator/CalculatorTest.java
+++ src/test/java/com/mchourreau/calculator/CalculatorTest.java
@@ -1,7 +1,28 @@
 package com.mchourreau.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void add_devrait_calculer_la_somme_de_deux_int(){
+
+        assertEquals(5, calculator.add(2, 3)); // 2 + 3 = 5
+
+    }
+
+    @Test
+    public void divide_devrait_calculer_la_division_de_deux_entier() {
+        assertEquals(2, calculator.divide(6, 3)); // 6 / 3 = 2
+    }
 
 }
